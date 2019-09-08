74a5b7cec75ab740e01e1e6b583822a03d489557
Ihor Didyk <ihordidyk95@gmail.com>
2019-09-08 21:55:15 +0300

    created component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { ChatFeedComponent } from './chat-feed/chat-feed.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent,
+    ChatRoomComponent,
+    ChatFeedComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    UserComponent,
+    UserListComponent,
+    NavbarComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +31,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
